6032f1dc3b2c00eddc2e534458e90c9c4ab6e1d5
makif34 <sanlanakif32@gmail.com>
2023-07-25 12:00:19 +0300

    25 temmuz kayıt sistemi mantığı

---
+++ GameProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            gamerManager.Add(new Gamer
+            {
+                Id = 1,
+                BirthYear = 1985,
+                Firstname = "Akif",
+                LastName = "Şanlan",
+                IdentityNumber = 12345
+              
+            });
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SQLQuery5.sql
@@ -0,0 +1,26 @@
+﻿--Select
+Select ContactName Adi, CompanyName Sirkeadi,City Sehir from Customers
+
+Select *from Customers where City ='Berlin'
+
+Select* from Products where categoryId=1 or CategoryID=3
+Select* from Products where categoryId=1 or UnitPrice>=10
+Select * from Products order by ProductName--sıraladık alfabetik olarak
+Select * from Products order by ProductName
+select * from Products order by UnitPrice desc
+
+Select count (*) Adet from Products 
+Select categoryId, count(*) from products where UnitPrice>20 group by CategoryId having count(*)<10
+
+select Products.ProductID, Products.ProductName, Products.UnitPrice,Categories.CategoryName
+from Products inner join Categories 
+on Products.CategoryID= Categories.CategoryID
+where Products.UnitPrice>10
+select * from Products p left join [Order Details] od
+on p.ProductID= od.ProductID
+inner join Orders o
+on o.OrderID=od.OrderID
+
+select * from Customers c left join Orders o
+on c.CustomerID=o.CustomerID
+where o.CustomerID is null
